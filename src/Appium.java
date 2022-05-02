dd0627d06bbf8c85d6453f90007543225f94e816
Shubham <lionshubham1995@gmail.com>
2022-05-02 07:16:10 +0530

    develop work start

---
+++ src/Appium.java
@@ -0,0 +1,10 @@
+
+public class Appium {
+
+	public static void main(String args[])
+	{
+		System.out.println("hello appium");
+		System.out.println("what are you doing");
+		System.out.println("are you busy");
+	}
+}
