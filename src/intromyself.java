0d35bfb4037474551804d9b8701a5abf56442da2
Shubham <lionshubham1995@gmail.com>
2022-05-02 07:28:52 +0530

    develop work was good

--- src/intromyself.java
+++ src/intromyself.java
@@ -14,6 +14,8 @@ System.out.println("I am doing good");
 System.out.println("I am missing you");
 System.out.println("Whats about you ?");
 System.out.println("When will you come ?");
+System.out.println("app1?");
+System.out.println("app2?");
 	}
 
 }
